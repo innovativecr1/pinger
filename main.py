ec66db8c248138695b489284da0ae5b0364c14b7
Mayank <innovativecreations195@gmail.com>
2025-01-27 11:59:19 +0530

    using selenium

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # import requests
 # from bs4 import BeautifulSoup
-# # url = "https://www.youtube.com/@SamayRainaOfficial/videos"
-# url = "/index.html"
+# url = "https://www.youtube.com/@SamayRainaOfficial/about"
 
-# data = requests.get(url)
+
+# headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+#     }
+
+# data = requests.get(url=url, headers=headers)
+
+
 
 # t = data.text
 # soup = BeautifulSoup(t,'html.parser')
@@ -11,23 +17,47 @@
 # d = soup.prettify()
 
 # print(d)
-# # with open('index.html', 'w', encoding='utf-8') as file:
-# #     file.write(d)
+# with open('index.html', 'w', encoding='utf-8') as file:
+#     file.write(d)
 
    
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-url = "https://www.youtube.com/@SamayRainaOfficial/videos"
-data = requests.get(url)
+# url = "https://www.youtube.com/@SamayRainaOfficial/videos"
 
-t = data.text
-soup = BeautifulSoup(t,'html.parser')
+# headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+#     }
 
-d = soup.prettify()
-all_tags = soup.find_all(True)
-tag_count = len(all_tags)
+# data = requests.get(url, headers=headers)
 
-tag = soup.find("yt-content-metadata-view-model")
 
-print(tag)
+
+# t = data.text
+# soup = BeautifulSoup(t,'html.parser')
+
+# d = soup.prettify()
+# all_tags = soup.find_all(True)
+# tag_count = len(all_tags)
+
+# tag = soup.find("yt-content-metadata-view-model")
+
+# print(tag)
+
+############### Approch - 2 ##############################
+
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.youtube.com/@SamayRainaOfficial/videos")
+
+title = driver.title
+print(title)
+
+driver.quit()
