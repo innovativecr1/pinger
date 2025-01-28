9c61464842e8a000481fd62fba0bb6a97122a35a
Mayank <innovativecreations195@gmail.com>
2025-01-28 11:14:06 +0530

    learnt required json operations

--- using_json.py
+++ using_json.py
@@ -1,20 +1,20 @@
-import json
-
-with open('data.json') as f:
-    data = json.load(f)
-
-print(data)
-
-
 # import json
 
-# data = {
-#     'name': 'Bob',
-#     'age': '25',
-#     'city': 'Los Angeles'
-# }
-
 # with open('data.json') as f:
-#     d = json.load(f)
-#     print(d)
-#     print(d['name'])
+#     data = json.load(f)
+
+# print(data)
+
+
+import json
+
+data = {
+    'videos': 356
+}
+m = open("data.json")
+
+d = json.load(m)
+print(d["videos"])
+
+n = open("data.json", "w")
+json.dump(data, n)
